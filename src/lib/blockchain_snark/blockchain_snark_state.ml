fc59121021c3c3c06e15ad3f680724b1dbfbeb1b
dkijania <dariusz@o1labs.org>
2024-10-19 10:54:14 +0200

    make change in src

--- src/lib/blockchain_snark/blockchain_snark_state.ml
+++ src/lib/blockchain_snark/blockchain_snark_state.ml
@@ -425,6 +425,7 @@ let rule ~proof_level ~constraint_constants transaction_snark self :
   ; feature_flags = Pickles_types.Plonk_types.Features.none_bool
   }
 
+
 module type S = sig
   module Proof :
     Pickles.Proof_intf
